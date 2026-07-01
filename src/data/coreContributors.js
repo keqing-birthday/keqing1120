c09f21a98ac5215c99009c1d704dc36e3f53ed6e
玉衡星织 <dev@keqing-birthday.local>
2026-07-01 15:49:59 +0800

    我家晴宝不可能这么可爱

--- src/data/coreContributors.js
+++ src/data/coreContributors.js
@@ -9,7 +9,7 @@
  * - bilibili：B站个人空间链接，留空或 '#' 表示暂不跳转
  */
 export const coreContributors = [
-  { name: '玉衡星家的厨子(刻晴厨)', role: '主办', avatar: '/images/direct.webp', bilibili: 'https://space.bilibili.com/3546695900072565' },
+  { name: '我家晴宝不可能这么可爱)', role: '主办', avatar: '/images/direct.webp', bilibili: 'https://space.bilibili.com/3546695900072565' },
   { name: '三代', role: '三维动画与导播', avatar: '/images/b_bfa0f2a5341561b777411c5044f160e9.jpg', bilibili: 'https://space.bilibili.com/1418606' },
   { name: 'ink ECY', role: '剧场与同人作者', avatar: '/images/b_b1c20e6b96481e6271ac9cc1404061e1.jpg', bilibili: 'https://space.bilibili.com/1740529490' },
   { name: '一条梦醒的鱼', role: '剪辑/七圣召唤', avatar: '/images/f377c50438d10e713facbc468e41310a.png', bilibili: 'https://space.bilibili.com/348817613' },
